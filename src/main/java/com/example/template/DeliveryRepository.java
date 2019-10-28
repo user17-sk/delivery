25b0304f19585459ef1725fe50757e5e5e84e5af
kimscott <sanaloveyou@uengine.org>
2019-10-28 13:34:39 +0900

    add api

--- src/main/java/com/example/template/DeliveryRepository.java
+++ src/main/java/com/example/template/DeliveryRepository.java
@@ -2,6 +2,10 @@ package com.example.template;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
+
+    List<Delivery> findByOrderIdOrderByDeliveryIdDesc(Long orderId);
 
 }
